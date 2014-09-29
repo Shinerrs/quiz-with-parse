78c9946d31138dc2e37e0b312f218826fc26e098
Supratim Chowdhury <supratims@gmail.com>
2014-09-29 16:42:27 +0100

    added profile page

--- cloudfn/cloudapi/cloud/main.js
+++ cloudfn/cloudapi/cloud/main.js
@@ -8,11 +8,12 @@ Parse.Cloud.define("nextRandomQuestion", function(request, response) {
   query.equalTo("userId", request.params.localuserid);
   query.find({
     success: function(results) {
-    	console.log(results);
+    	getQuestion(results);
+    	//console.log(results);
     	if (results.length == 0) {
-    		createUserActivity(getQuestion);
+    		//createUserActivity(getQuestion);
     	} else {
-    		getQuestion(results);
+    		//getQuestion(results);
     	}
     },
     error: function(error) {
@@ -21,11 +22,15 @@ Parse.Cloud.define("nextRandomQuestion", function(request, response) {
   });
 
   function getQuestion(userActivities) {
-	console.log("getQuestion");
 	var doneQuesIds = [];
 	if (userActivities && userActivities.length > 0) {
-		doneQuesIds = userActivities.map(function(d) { return d.questionId });
+		for (var i=0; i<userActivities.length; i++) {
+			doneQuesIds.push(userActivities[i].get('questionId'));
+		}
 	}
+
+	//console.log(doneQuesIds);
+
 	var quesQuery = new Parse.Query("IndiaQuiz");
 	doneQuesIds.length > 0 && quesQuery.notContainedIn('objectId', doneQuesIds);
 	quesQuery.limit(1);
@@ -58,9 +63,9 @@ Parse.Cloud.define("nextRandomQuestion", function(request, response) {
 
 Parse.Cloud.define("checkAnswer", function(request, response) {
 	var IndiaQuiz = Parse.Object.extend("IndiaQuiz");
-	var query = new Parse.Query(IndiaQuiz);	
-	
-	query.equalTo("objectId", request.params.objectId);	
+	var query = new Parse.Query(IndiaQuiz);
+
+	query.equalTo("objectId", request.params.objectId);
 	query.first({
 		success: function(object) {
 			response.success(object);
@@ -74,17 +79,48 @@ Parse.Cloud.define("checkAnswer", function(request, response) {
 Parse.Cloud.define("updateUserActivity", function(request, response) {
 	var UserActivity = Parse.Object.extend("UserActivity");
 	var userActivity = new UserActivity();
-	 
+
 	userActivity.set("userId", request.params.localuserid);
-	userActivity.set("questionId", request.params.questionId);	
-	 
+	userActivity.set("questionId", request.params.questionId);
+	userActivity.set("score", ''+request.params.score);
+
 	userActivity.save(null, {
-	  success: function(newobj) {	    
-	  	response.success("success");  
+	  success: function(newobj) {
+	  	response.success("success");
 	  },
-	  error: function(obj, error) {	    
+	  error: function(obj, error) {
 	  	console.log(error);
+	  	response.error(error.code);
 	  }
 	});
 
+});
+
+
+Parse.Cloud.define("fetchUserActivity", function(request, response) {
+	var query = new Parse.Query("UserActivity");
+	  query.equalTo("userId", request.params.localuserid);
+	  query.find({
+	    success: function(results) {
+	    	var obj = {right: 0, wrong: 0, perc: 0};
+	    	var tot = 0;
+	    	if (results && results.length > 0) {
+			  for (var i=0; i< results.length; i++) {
+			  	if (results[i].get("questionId") !== '') {
+					if (results[i].get('score') == 0) {
+					    obj.wrong ++;
+					} else {
+						obj.right ++;
+					}
+					tot ++;
+			  	}
+			  }
+	    	}
+	    	if (tot != 0) obj.perc = Math.round(obj.right*100/tot, 2);
+	    	response.success(obj);
+	    },
+	    error: function() {
+	      response.error("Profile lookup failed");
+	    }
+    });
 });
--- js/load-json.js
+++ js/load-json.js
@@ -4,9 +4,12 @@ var userId = LocalStorageStore.getUserId();
 function getNextQuestion(callback) {
 	Parse.Cloud.run('nextRandomQuestion', {localuserid: userId}, {
 	  success: function(resp) {
-	     console.log($.extend({objectId: resp.id}, resp.attributes));
-	     callback($.extend({objectId: resp.id}, resp.attributes));
-
+	  	 if (resp) {
+			 //console.log($.extend({objectId: resp.id}, resp.attributes));
+			 callback($.extend({objectId: resp.id}, resp.attributes));
+	  	 } else {
+			 $('#mainbody').html(Mustache.render($('#error-tmpl').html(), {msg: 'No quiz available. Please come back later.'}));
+	  	 }
 	  },
 	  error: function(error) {
 		//console.log(error);
@@ -17,7 +20,7 @@ function getNextQuestion(callback) {
 function getAnswer(userId, objectId, callback) {
 	Parse.Cloud.run('checkAnswer', {localuserid: userId, objectId: objectId}, {
 	  success: function(resp) {
-	     console.log(resp);
+	     //console.log(resp);
 	     callback(resp.attributes);
 
 	  },
@@ -27,8 +30,8 @@ function getAnswer(userId, objectId, callback) {
 	});
 }
 
-function updateUserActivity(userId, questionId, callback) {
-	Parse.Cloud.run('updateUserActivity', {localuserid: userId, questionId: questionId}, {
+function updateUserActivity(userId, questionId, rightAnswer, callback) {
+	Parse.Cloud.run('updateUserActivity', {localuserid: userId, questionId: questionId, score: rightAnswer ? 1 : 0}, {
 	  success: function(resp) {
 	     //console.log(resp._serverData);
 	     callback && callback(resp._serverData);
@@ -53,12 +56,12 @@ $(document).ready(function(){
 					answer: r.answer,
 					objectId: r.objectId
 				},
-				list: [{id: 1, questionId: r.objectId, title: r.option1}, 
-					{id: 2, questionId: r.objectId, title: r.option2}, 
-					{id: 3, questionId: r.objectId, title: r.option3}, 
+				list: [{id: 1, questionId: r.objectId, title: r.option1},
+					{id: 2, questionId: r.objectId, title: r.option2},
+					{id: 3, questionId: r.objectId, title: r.option3},
 					{id: 4, questionId: r.objectId, title: r.option4}]
 			};
-			
+
 			$('#mainbody').html(Mustache.render(tmpl, obj));
 		});
 	}
@@ -66,19 +69,44 @@ $(document).ready(function(){
 
 	$('#submitAnswer').live('click', function() {
 		var ans = $('input[name=group]:checked').data('id');
-		var questionId = $('input[name=group]:checked').data('questionid');		
-		console.log("Clicked: " + ans + ", " + questionId );
-		updateUserActivity(userId, questionId, function(o) {
-			//
-		});
+		var questionId = $('input[name=group]:checked').data('questionid');
+		//console.log("Clicked: " + ans + ", " + questionId );
+
 		getAnswer(userId, questionId, function(o) {
 			//console.log(o);
-			if (ans == o.answer) {				
+			if (ans == o.answer) {
 				alert("Right !");
 			} else {
 				alert("Wrong !");
 			}
-			nextQuestion();
+			updateUserActivity(userId, questionId, ans == o.answer, function(o) {
+				nextQuestion();
+			});
+
 		});
+	});
+
+	function showProfile(obj) {
+		$('#titlebar').html(Mustache.render($('#profile-titlebar-tmpl').html(), {title: 'Profile'}));
+		$('#mainbody').html(Mustache.render($('#profile-tmpl').html(), obj));
+	}
+
+	$('#profileLink').live('click', function() {
+		Parse.Cloud.run('fetchUserActivity', {localuserid: userId}, {
+			success: function(resp) {
+				console.log(resp);
+				resp ? showProfile(resp) : showProfile({});
+			},
+			error: function(error) {
+				showProfile({});
+			}
+		});
+
+	});
+
+
+	$('#playLink').live('click', function() {
+		$('#titlebar').html(Mustache.render($('#gen-titlebar-tmpl').html(), {title: 'Play'}));
+		nextQuestion();
 	});
 });
